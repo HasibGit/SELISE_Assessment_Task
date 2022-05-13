355eb154951d686d0cae13ff0574ba5a0adb34c2
Hasib Ullah <walisantunu@gmail.com>
2022-05-13 23:13:50 +0600

    Loading spinner added while fetching data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Selise_Assessment';
+
+  showLoadingIndicator = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((routerEvent: Event) => {
+      if (routerEvent instanceof NavigationStart) {
+        this.showLoadingIndicator = true;
+      }
+
+      if (routerEvent instanceof NavigationEnd) {
+        this.showLoadingIndicator = false;
+      }
+    });
+  }
 }
